665f872c817bb6e2619373209216d5aaad4aea0b
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-14 22:03:09 +0530

    Updated App.js for carModel route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const carModelRoute = require('./routes/carModel');
+app.use('/carModel', carModelRoute);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
